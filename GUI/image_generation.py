caf93a3575989fa3a95644f0ec9640cf92586d8b
Varun Kamat <varunkamat@berkeley.edu>
2021-04-28 11:06:42 -0700

    Got interactive version working, woohoo

--- GUI/image_generation.py
+++ GUI/image_generation.py
@@ -19,8 +19,8 @@ import mpl_toolkits
 # plant species. the x dimension specifies plant type, the y and z specify the location of the plant,
 # and the value entry represents the height of the plant.
 
-garden_x_len = 8
-garden_y_len = 15
+garden_x_len = 10
+garden_y_len = 10
 garden_z_len = 5
 garden_dimensions = (garden_x_len, garden_y_len, garden_z_len)
 colors_of_plants = ["red", "yellow", "green", "blue"]
@@ -63,12 +63,12 @@ def plot_and_show_images(leftimage, rightimage, root):
     figs = []
     axes = []
     for _ in range(len(images_to_process)):
-        #fig = Figure(figsize=(5, 4), dpi=100)
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
+        fig = Figure(figsize=(5, 4), dpi=100)
         figs.append(fig)
+    canvases = pack_images(root, figs)
+    for fig in figs:
+        ax = fig.add_subplot(projection='3d')
         axes.append(ax)
-
     for ax in axes:
         #Axes3D only supports aspect arg 'auto'
         #ax.set_aspect(aspect=1)
@@ -135,19 +135,16 @@ def plot_and_show_images(leftimage, rightimage, root):
                                 cstride=cstride, color=color, shade=False)
                 ax.plot_surface(circ_x, circ_y_2, circ_z_2, alpha=alpha, rstride=rstride,
                                 cstride=cstride, color=color, shade=False)
+    #pack_toolbars(root, canvases)
 
-        if root:
-            pack_images(root, figs, ax)
-        else:
-            plt.show()
-
-def pack_images(root, figs, ax):
+def pack_images(root, figs):
     for slave in root.pack_slaves():
         slave.pack_forget()
-
+    canvases = []
     for count, fig in enumerate(figs):
         canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
         canvas.draw()
+        canvases.append(canvas)
 
         # a somewhat hack-y solution to get the left image to pack on the left and the right image to
         # pack on the right.
@@ -155,4 +152,9 @@ def pack_images(root, figs, ax):
             canvas.get_tk_widget().pack(side="left")
         else:
             canvas.get_tk_widget().pack(side="right")
-        ax.mouse_init()
+    return canvases
+
+def pack_toolbars(root, canvases):
+    for canvas in canvases:
+        toolbar = NavigationToolbar2Tk(canvas, root)
+        toolbar.update()
