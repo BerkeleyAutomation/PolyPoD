99a23a4c14048ead7c5adda5d781b8e7a92f365b
Varun Kamat <varunkamat@berkeley.edu>
2021-04-30 09:15:23 -0700

    initial commit on visual-updates-4-30: wider plants, more of them, color pallete, removing grid

--- GUI/image_generation.py
+++ GUI/image_generation.py
@@ -15,16 +15,17 @@ from matplotlib.figure import Figure
 from matplotlib.backend_bases import key_press_handler
 import mpl_toolkits
 
-# Generates random 4x10x10 (garden_z_len by g...) images of dots and cirles, representing a 10x10 garden with 4 different
+# Generates random 4x10x10 (plant_max_height by g...) images of dots and cirles, representing a 10x10 garden with 4 different
 # plant species. the x dimension specifies plant type, the y and z specify the location of the plant,
 # and the value entry represents the height of the plant.
 
 garden_x_len = 10
 garden_y_len = 10
-garden_z_len = 5
-garden_dimensions = (garden_x_len, garden_y_len, garden_z_len)
+plant_max_height = 2
+plant_min_height = 0.5
+garden_dimensions = (garden_x_len, garden_y_len, plant_max_height)
 colors_of_plants = ["red", "yellow", "green", "blue"]
-num_plants = 4
+num_plants = 10
 prob_of_plant = 0.25
 alpha = 1
 
@@ -33,8 +34,9 @@ alpha = 1
 def plant_radius():
     return rng.uniform(0.25, 1)
 
-def plant_height():
-    return rng.uniform(1, garden_z_len)
+def plant_height(plant_type):
+
+    return rng.uniform(plant_min_height, plant_max_height)
 
 # Returns plant x, y, z, and radius from an iterator with multi-index, and the value returned by
 # the iterator.
@@ -56,8 +58,9 @@ def generate_random_images():
         it = np.nditer(plant_present, flags=["multi_index"])
         for x in it:
             if x == 1:
-                plant_matrix[it.multi_index[0], it.multi_index[1], rng.integers(4)] = \
-                    plant_height()
+                plant_type = rng.integers(num_plants)
+                plant_matrix[it.multi_index[0], it.multi_index[1], plant_type] = \
+                    plant_height(plant_type)
         r.append(plant_matrix)
     return r
 
@@ -71,13 +74,21 @@ def plot_and_show_images(leftimage, rightimage, root):
     canvases = pack_images(root, figs)
     for fig in figs:
         ax = fig.add_subplot(projection='3d')
+        # Hide grid lines
+        ax.grid(False)
+
+        # Hide axes ticks
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_zticks([])
+
         axes.append(ax)
     for ax in axes:
         #Axes3D only supports aspect arg 'auto'
         #ax.set_aspect(aspect=1)
         ax.set_xlim(left=-1, right=garden_x_len)
         ax.set_ylim(bottom=-1, top=garden_y_len)
-        ax.set_zlim(bottom=0, top=garden_z_len)
+        ax.set_zlim(bottom=0, top=plant_max_height)
     for _ in range(len(images_to_process)):
         img = images_to_process.pop()
         ax = axes.pop()
