fe96ce669f1393aef36426cef03a88b81766fff6
Varun Kamat <varunkamat@berkeley.edu>
2021-05-13 11:59:09 -0700

    final earthcolor version of vrpd with 3 of each type of plant

--- GUI/plot_poisson_disk.py
+++ GUI/plot_poisson_disk.py
@@ -5,14 +5,18 @@ import numpy as np
 
 r = 0.25
 dims = np.array([garden_constants.garden_x_len, garden_constants.garden_y_len])
-num_of_each_plant = np.full(garden_constants.num_plants, 2)
+num_of_each_plant = np.full(garden_constants.num_plants, 3)
 plant_radii = garden_constants.plant_height_distribution_params
 cellsize=0.1
 data = poi.vrpd(dims, num_of_each_plant, plant_radii, cellsize)
 ax = plt.gca()
 for p in data:
-    ax.add_patch(plt.Circle(p, r, color='b', fill=False, clip_on=False))
-datax, datay = data[:,0], data[:,1]
+    loc, plant_index = p
+    r = garden_constants.plant_height_distribution_params[plant_index]
+    color = garden_constants.colors_of_plants[int(plant_index)]
+    ax.add_patch(plt.Circle(loc, r, color=color, fill=False, clip_on=False))
+locdata = np.array([x[0] for x in data])
+datax, datay = locdata[:,0], locdata[:,1]
 plt.scatter(datax, datay, s=1, color='k')
 ax.set_aspect(1)
 plt.show()
--- GUI/poisson_disk/poisson_disc.py
+++ GUI/poisson_disk/poisson_disc.py
@@ -13,8 +13,6 @@ import math
 from numpy.random import default_rng
 rng = default_rng()
 
-def plant_radius(plant_type):
-    return garden_constants.plant_height_distribution_params[plant_type]
 
 # Uniform sampling in a hypersphere
 # Based on Matlab implementation by Roger Stafford
@@ -46,18 +44,24 @@ def squared_distance(p0, p1):
 
 
 def vrpd(dims, num_of_each_plant, plant_radii, cellsize):
+    ndim = dims.size
     dims = (dims / cellsize).astype(int)
-    ndim=dims.size
+    def inhibition_radius(plant_type):
+        return garden_constants.plant_height_distribution_params[plant_type] / cellsize
     def next_point(plant_type):
-        r = plant_radius(plant_type)
-        criteria = (np.isnan(plant_map(points)) & (cp_map(points) <= r))
+        r = inhibition_radius(plant_type)
+        pm = plant_map(points)
+        cpm = cp_map(points)
+        c1 = np.isnan(pm)
+        c2 = cpm >= r
+        criteria = (c1 & c2)
         candidates = points[criteria]
         if len(candidates) == 0:
             return False
         choice = candidates[rng.integers(candidates.shape[0])]
-        add_point(point_coords(choice), r)
+        add_point(point_coords(choice), r, plant_type)
         return True
-    def add_point(choice, r):
+    def add_point(choice, r, plant_type):
         x, y = choice
         pl = point_list(points)
         for p in pl:
@@ -67,30 +71,36 @@ def vrpd(dims, num_of_each_plant, plant_radii, cellsize):
                 set_cp(px, py, d)
             if d < r:
                 mark_blocked(px, py)
-        mark_plant(x, y)
+        mark_plant(x, y, plant_type)
 
     # points stores point locations:
-    # points[x, y] = [b, x, y, cp]
+    # points[x, y] = [b, x, y, cp, t]
     # b = 1.0 if plant here, 2.0 if point is blocked, np.nan if no plant.
     # x, y is loc of plant
     # cp is distanct to closest plant
-    points = np.full(np.append(dims, ndim + 2), np.nan, dtype=np.float32)
+    # t is type of plant, as plant index
+    points = np.full(np.append(dims, ndim + 3), np.nan, dtype=np.float32)
     it = np.nditer(points, flags=["multi_index", "refs_ok"])
     for p in it:
         if it.multi_index[2] == 1:
             points[it.multi_index] = it.multi_index[0]
         elif it.multi_index[2] == 2:
             points[it.multi_index] = it.multi_index[1]
+        elif it.multi_index[2] == 3:
+            points[it.multi_index] = np.inf
     def plant_map(points):
         return points[:,:,0]
     def point_coords(p):
         return p[1:3]
     def cp(p):
-        return p[2]
+        return p[3]
+    def plant_type(p):
+        return p[4]
     def set_cp(x, y, cp):
-        points[int(x), int(y), 2] = cp
-    def mark_plant(x, y):
+        points[int(x), int(y), 3] = cp
+    def mark_plant(x, y, t):
         points[int(x), int(y), 0] = 1.0
+        points[int(x), int(y), 4] = t
     def mark_blocked(x, y):
         points[int(x), int(y), 0] = 2.0
     def cp_map(points):
@@ -100,7 +110,8 @@ def vrpd(dims, num_of_each_plant, plant_radii, cellsize):
 
     x, y = uniform_sample(ndim, dims)
     plant_index = garden_constants.num_plants - 1
-    add_point([x, y], plant_radius(plant_index))
+    add_point([x, y], inhibition_radius(plant_index), plant_index)
+    num_of_each_plant[plant_index] -= 1
     while plant_index >= 0:
         for _ in range(num_of_each_plant[plant_index]):
             n = next_point(plant_index)
@@ -111,6 +122,6 @@ def vrpd(dims, num_of_each_plant, plant_radii, cellsize):
     # to cartesian
     plant_map = plant_map(points)
     final_points = points[plant_map == 1.0]
-    final_points = np.array([point_coords(x) for x in final_points]) * cellsize
+    final_points = np.array([np.array([point_coords(x) * cellsize, plant_type(x)], dtype=object) for x in final_points])
     return final_points
 
