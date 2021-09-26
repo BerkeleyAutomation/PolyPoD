fe55db4ade437c0cc80025b87da63b4369ead600
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2021-09-26 08:40:53 -0700

    about to mess up load data

--- GUI/load_data.py
+++ GUI/load_data.py
@@ -1,4 +1,19 @@
 import pickle
+import os
+import plotly_test as pt
+import time
 
-x = pickle.load(open("datasets/dataset7/7_0_data", 'rb'))
-pass
+directory = "datasets/dataset5"
+for entry in os.scandir(directory):
+    if (entry.path.endswith("data") and entry.is_file()):
+        x = pickle.load(open(entry.path, 'rb'))
+        g_data = x['data']
+        new_filename = entry.path + "_no_voids"
+        pt.plotly_test(pt.single_values['y_eye_mult'],
+                       pt.single_values['z_ratio'],
+                       pt.single_values['h_mult'],
+                       pt.single_values['color_dict'],
+                       cylinder_nt=70,
+                       data=g_data, plant_labels=False, save=True,
+                       where_to_save=new_filename, void_size=0)
+        time.sleep(5)
