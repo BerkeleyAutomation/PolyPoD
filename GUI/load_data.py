a9c38540299e697ff023198323f07f3417a98d39
Varun Kamat <67357457+varunthepianoman@users.noreply.github.com>
2021-10-08 19:29:22 -0700

    load data nonimportant

--- GUI/load_data.py
+++ GUI/load_data.py
@@ -2,18 +2,14 @@ import pickle
 import os
 import plotly_test as pt
 import time
+import re
 
 directory = "datasets/dataset5"
+list = [x for x in range(1000)]
+print(list)
 for entry in os.scandir(directory):
     if (entry.path.endswith("data") and entry.is_file()):
-        x = pickle.load(open(entry.path, 'rb'))
-        g_data = x['data']
-        new_filename = entry.path + "_no_voids"
-        pt.plotly_test(pt.single_values['y_eye_mult'],
-                       pt.single_values['z_ratio'],
-                       pt.single_values['h_mult'],
-                       pt.single_values['color_dict'],
-                       cylinder_nt=70,
-                       data=g_data, plant_labels=False, save=True,
-                       where_to_save=new_filename, void_size=0)
-        time.sleep(5)
+        m = re.search("_[0-9]+_", entry.path)
+        num = m.group(0)[1:-1]
+        list.remove(int(num))
+print(list)
