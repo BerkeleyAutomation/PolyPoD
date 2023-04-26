e28797827cfdccfd7ddaac80500e21ac6d92fe27
Shrey A <Shreystechtips@gmail.com>
2023-04-25 18:04:15 -0700

    add api changes

--- GUI/app.py
+++ GUI/app.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask import Flask
 import os
+from flask_cors import CORS
 import sys
 from flask import request
 import numpy as np
@@ -9,9 +10,11 @@ from shapely.geometry import Polygon
 import plotting_utils
 import generate_test_gardens
 import bmca_utils
+import tempfile
 from flask import send_file
 
 app = Flask(__name__)
+CORS(app)
 
 
 num_trials = 1
@@ -24,11 +27,24 @@ save_2d=False
 
 
 
-@app.route(f'/get_parameters', methods = ["GET"])
+@app.route(f'/get_parameters', methods = ["POST"])
 def get_parameters():
     garden = {}
     
     #get parameters
+    request_json = request.get_json()
+    garden['density'] = request_json.get('density', 0.75)
+    garden['coordinates'] = request_json.get('coordinates', [])
+    garden['num_plants'] = request_json.get('num_plants', 0)
+    garden['num_each_plant'] = request_json.get('num_each_plant', [])
+    garden['distribution'] = 'even'
+    garden['beta'] = request_json.get('beta', 0.2)
+    garden['void_size'] = request_json.get('void_size', 15)
+    garden['void_number'] = request_json.get('void_number', 2)
+    garden['utility_func_exponent'] = request_json.get('utility_func_exponent', ['same', -6])
+    garden['symmetry'] = request_json.get('symmetry', 'neither')
+    
+    
     # garden["density"] = request.args.get('density')
     # garden["distribution"] = request.args.get('distribution')
     # garden["beta"] = request.args.get('beta')
@@ -40,23 +56,6 @@ def get_parameters():
     # garden["num_each_plant"] = np.full(9, 2) #change later
 
     #take user inputs
-    coordinates = [[100, 100], [200, 100], [200, 200], [300, 200], [300, 100], [400, 100], [400, 400],[250, 250], [250, 400], [100, 400], [100, 300], [175, 250], [100, 200], [100, 100]]
-    num_plants = 45
-    num_each_plant = [5, 5, 5, 5, 5, 5, 5, 5, 5]
-
-
-    # #dummy values
-    #garden["coordinates"] = [[100, 100], [200, 100], [200, 200], [300, 200], [300, 100], [400, 100], [400, 400],[250, 250], [250, 400], [100, 400], [100, 300], [175, 250], [100, 200], [100, 100]]
-    garden["coordinates"] = coordinates
-    garden["density"] = 0.75
-    garden["distribution"] = 'even'
-    garden["beta"] = 0.2
-    garden["void_size"] = 15
-    garden["void_number"] = 2
-    garden["utility_func_exponent"] = ['same', -6]
-    garden["symmetry"] = 'neither'
-    garden["num_each_plant"] = num_each_plant
-    garden["num_plants"] = num_plants
     garden = generate_test_gardens.add_d_to_garden(garden)
     print(garden)
 
@@ -64,9 +63,9 @@ def get_parameters():
                                                         cylinder_nt=cylinder_nt, data=None,
                                                         generate_plotly=generate_plotly, garden=garden,
                                                         save_plotly=save_plotly, save_2d=save_2d)
-    path = os.path.abspath("GUI")
-    fig.savefig(os.path.join(path, 'test.png'))
-    return send_file('test.png', mimetype='image/gif')
+    with tempfile.NamedTemporaryFile(suffix='.png') as f:
+        fig.savefig(f.name)
+        return send_file(f.name, mimetype='image/gif')
 
     
 if __name__ == "__main__":
